8d249863e7a4aa04d32ecfcb48749ceaa1036d3a
loumdt <mandonnet@tutanota.com>
2023-03-14 15:02:21 +0100

    delete stefanon

--- Code/E-OBS_use/Scan_Stefanon/stefanon_original/netcdf_load.m
+++
@@ -1,8 +0,0 @@
-filename = 'D:/Ubuntu/PFE/Data/E-OBS/0.1deg/temp_anomaly_summer_only_1950-2020.nc'
-var = ncread(filename,'temp');
-lat = ncread(filename,'lat');
-lon = ncread(filename,'lon');
-temps = ncread(filename,'time');
-var_name = ncreadatt(filename,'temp','long_name');
-var_unit = ncreadatt(filename,'temp','units');
-save -v7.3 D:/Ubuntu/PFE/Data/E-OBS/Detection_Canicule/tg_ano_JJA lat lon temps var var_name var_unit
--- Code/E-OBS_use/Scan_Stefanon/stefanon_original/netcdf_load_tn.m
+++
@@ -1,8 +0,0 @@
-filename = 'tn_EUR_MJJAS.nc'
-var = ncread(filename,'tn');
-lat = ncread(filename,'latitude');
-lon = ncread(filename,'longitude');
-temps = ncread(filename,'time');
-var_name = ncreadatt(filename,'tn','long_name');
-var_unit = ncreadatt(filename,'tn','units');
-save -v7.3 /home/ajeze/Documents/DetectionCanicule/tn lat lon temps var var_name var_unit
