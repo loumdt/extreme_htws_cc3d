9e1e02443b3d5dd168368203e2c8b7af90a9285e
loumdt <mandonnet@tutanota.com>
2026-04-16 18:03:09 +0200

    add memory issue fix for HWMId_pop

--- Code/Run_analysis/mk_senslope_CI.R
+++ Code/Run_analysis/mk_senslope_CI.R
@@ -9,11 +9,13 @@ file_path <- args[1]
 # Load data to compute trend on frequency
 data_frequency <- read_csv(paste(file_path,"/frequency.csv", sep=""), col_types = cols(Year = col_datetime(format = "%Y")))
 # Load data to compute other trends
-df_htws_step2 <- read_csv(paste(file_path,"/df_htws_step3.csv", sep="" ),
+df_htws_step3 <- read_csv(paste(file_path,"/df_htws_step3.csv", sep="" ),
     col_types = cols(...1 = col_skip(), Year = col_datetime(format = "%Y")))
 # Select subset of columns
-cols_to_exclude <- names(df_htws_step2) %in% c('Start Date', 'End Date', 'EM-DAT heatwaves', 'EM-DAT Total Deaths', 'HWMId_pop_mean', 'HWMId_mean', 'Temp_sum', 'Temp_pop', 'Total Exposed_population')
-sub_df <- df_htws_step2[!cols_to_exclude]
+cols_to_exclude <- names(df_htws_step3) %in% c('Start Date', 'End Date', 'EM-DAT heatwaves', 'EM-DAT Total Deaths', 'HWMId_pop_mean', 'HWMId_mean', 'Temp_sum', 'Temp_pop', 'Total Exposed_population')
+sub_df <- df_htws_step3[!cols_to_exclude]
+
+sub_df$HWMId_pop <- sub_df$HWMId_pop/1e6 # Divide by 1e6 to avoid memory issues
 # Create output data.frame
 result_mk <- data.frame(slope = numeric(), UCL = numeric(), LCL = numeric(), P = numeric(), ss = numeric())
 # Compute Mann-Kendall trend test on frequency
@@ -26,5 +28,8 @@ res <- MK.tempAggr(data=sub_df[,c(1,i)], resolution = 0.9)
 index_name <- colnames(sub_df[,i]) # Get index name and use it as output row name
 result_mk[index_name,] <- res
 }
+result_mk['HWMId_pop','slope'] <- result_mk['HWMId_pop','slope']*1e6 # Set back to orginal unit
+result_mk['HWMId_pop','UCL'] <- result_mk['HWMId_pop','UCL']*1e6 # Set back to orginal unit
+result_mk['HWMId_pop','LCL'] <- result_mk['HWMId_pop','LCL']*1e6 # Set back to orginal unit
 # Write result to json file
 write_json(result_mk,paste(file_path,"/mk_result.json",sep=""))
